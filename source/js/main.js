82f4ff3db9fe2e2e28d1c91128fbd053b3cf98fe
Ellie Hoshizaki <ellie@mediahound.com>
2015-02-08 19:17:26 -0800

    Adding artist name hover effects to homepage.

--- source/js/main.js
+++ source/js/main.js
@@ -4,4 +4,59 @@ $(document).ready(function(){
     $(".home-menu").slideToggle("slow");
   });
 
+
+  $(".artist-name:eq(0)").hover(
+    function(){
+      $(".gradient").removeClass("gradient-animate");
+      $(".gradient").addClass("artist-1");
+    }, function(){
+      $(".gradient").removeClass("artist-1");
+      $(".gradient").addClass("gradient-animate");
+    }
+  );
+
+  $(".artist-name:eq(1)").hover(
+    function(){
+      $(".gradient").removeClass("gradient-animate");
+      $(".gradient").addClass("artist-2");
+    }, function(){
+      $(".gradient").removeClass("artist-2");
+      $(".gradient").addClass("gradient-animate");
+    }
+  );
+
+  $(".artist-name:eq(2)").hover(
+    function(){
+      $(".gradient").removeClass("gradient-animate");
+      $(".gradient").addClass("artist-3");
+    }, function(){
+      $(".gradient").removeClass("artist-3");
+      $(".gradient").addClass("gradient-animate");
+    }
+  );
 });
+
+var homeBgHover = function(){
+  var i,
+  artistName = document.getElementsByClassName("artist-name");
+
+  artistName[0].hover(
+      function(){
+        $(".gradient").addClass("'artist-1");
+      }, function(){
+        $(".gradient").removeClass("'artist-1");
+      }
+    )
+
+  // console.log(artistName.length);
+  // for(i=0;i<artistName.length-1;i++){
+  //   console.log(i);
+  //   artistName[i].hover(
+  //     function(){
+  //       $(".gradient").addClass("'artist-'+[i]");
+  //     }, function(){
+  //       $(".gradient").removeClass("'artist-'+[i]");
+  //     }
+  //   )
+  // }
+}
